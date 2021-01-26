d59fba557d8bd8d5379279987f4e47fa56733305
Krishna Patil <krishnapatil1406@gmail.com>
2021-01-26 15:11:08 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $('.btn').on('click', function (e) {
         const hash = this.hash;
         $('html, body').animate(
             {
-                scrollTop: $(hash).offset().top - 20
+                scrollTop: $(hash).offset().top
             },
             800
         );
